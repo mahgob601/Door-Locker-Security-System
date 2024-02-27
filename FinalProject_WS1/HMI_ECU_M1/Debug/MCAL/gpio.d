0281dbf8c2f4354b4ec8a50cdc66801a11306ff9
Yassin Mahgoub <mahgob601@gmail.com>
2024-02-27 10:42:19 +0200

    Delete FinalProject_WS1/HMI_ECU_M1 directory

--- FinalProject_WS1/HMI_ECU_M1/Debug/MCAL/gpio.d
+++
@@ -1,8 +0,0 @@
-MCAL/gpio.o MCAL/gpio.o: ../MCAL/gpio.c ../MCAL/gpio.h \
-  ../MCAL/../std_types.h ../MCAL/../common_macros.h
-
-../MCAL/gpio.h:
-
-../MCAL/../std_types.h:
-
-../MCAL/../common_macros.h:
--- FinalProject_WS1/HMI_ECU_M1/Debug/MCAL/timer1.d
+++
@@ -1,6 +0,0 @@
-MCAL/timer1.o MCAL/timer1.o: ../MCAL/timer1.c ../MCAL/timer1.h \
-  ../MCAL/../std_types.h
-
-../MCAL/timer1.h:
-
-../MCAL/../std_types.h:
